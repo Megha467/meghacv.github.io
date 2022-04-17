350f0f7edcbe9ff7ada101461feba3aeadc3026d
Megha <kumari123vivek@gmail.com>
2022-04-17 09:54:19 -0700

    Extra files deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.js';
 import CV from './components/CV';
-import Contact from './components/Contact';
 function App() {
   return (
     <div>
